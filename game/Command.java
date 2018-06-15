2562e9c0feb7b2c5577e5347c908070ec52c0390
Chen <victorchennn@gmail.com>
2018-06-15 15:45:00 +0800

    regex for some types

--- game/Command.java
+++ game/Command.java
@@ -3,31 +3,47 @@ package game;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/** All things to do with parsing commands.
+ *  @author Victor
+ */
 public class Command {
 
-    static enum Type {
+    /** Command types.  PIECE indicates a new piece of the form
+     *  %d, %d.  ERROR indicates a parse error in the command.
+     *  All other commands are upper-case versions of what the
+     *  programmer writes. */
+     static enum Type {
 
+        /* Start-up state only. */
         AUTO(""),
         MANUAL(""),
         START,
         SETBOARD(""),
-        Piece("[1-15],[1-15]"),
-        QUIT,
-        STATUS,
-        CLEAR,
-        PRINT,
-        HELP,
+        /* Regular pieces (set-up or play) */
+        PIECE("[1-15],[1-15]"),
+        /* Valid at any time. */
+        QUIT, STATUS, CLEAR, PRINT, HELP,
+        /* Special "commands" internally generated. */
+        /** Syntax error in command. */
         ERROR(".*"),
+        /** End of input stream. */
         EOF;
 
+        /** PATTERN is a regular expression string giving the syntax of
+         *  a command of the given type.  It matches the entire command,
+         *  assuming no leading or trailing whitespace.  The groups in
+         *  the pattern capture the operands (if any). */
         Type(String pattern) {
             _pattern = Pattern.compile(pattern + "$");
         }
 
+        /** A Type whose pattern is the lower-case version of its name. */
         Type() {
             _pattern = Pattern.compile(this.toString().toLowerCase());
         }
 
+        /** The Pattern describing syntactically correct versions of this
+         *  type of command. */
         private final Pattern _pattern;
     }
 
@@ -37,6 +53,7 @@ public class Command {
         _operands = operands;
     }
 
+    /** Parse COMMAND, returning the command and its operands. */
     static Command parseCommand(String command) {
         if (command == null) {
             return new Command(Type.EOF);
