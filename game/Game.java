33457b1e31fe8f5a8f6877370f9e7d14ec20eb3f
Chen <victorchennn@gmail.com>
2018-06-29 16:13:29 +0800

    distinguish using gui or not

--- game/Game.java
+++ game/Game.java
@@ -1,19 +1,22 @@
 package game;
 
 import java.awt.Desktop;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.function.Consumer;
+import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import static game.PieceColor.*;
 import static game.Game.State.*;
@@ -145,51 +148,82 @@ class Game {
     /** Perform the command 'save OPERANDS[0]'. */
     private void doSave(String[] operands) {
         try {
-            InputStream helpIn =
-                    Game.class.getClassLoader().getResourceAsStream(operands[0] + ".game");
-            if (helpIn != null) {
-                System.err.println("The name \"" + operands[0] +
-                        "\" is already taken." +
-                        " Please choose a different name.");
-            } else {
-                File file = new File(operands[0] + ".game");
-                FileWriter fw = new FileWriter(file, true);
-                String board = "";
-                for (int i = 0; i < SIDE * SIDE; i++) {
-                    board += _board.get(i).shortName();
+            File file = null;
+            if (_gui == null) {
+                InputStream helpIn =
+                        Game.class.getClassLoader().getResourceAsStream(operands[0] + ".game");
+                if (helpIn != null) {
+                    System.err.println("The name \"" + operands[0] +
+                            "\" is already taken." +
+                            " Please choose a different name.");
+                } else {
+                    file = new File(operands[0] + ".game");
                 }
-                fw.write(board + "\n");
-                fw.write(_board.whoseMove().shortName());
-                fw.flush();
-                fw.close();
+            } else {
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                        "game documents", "game");
+                chooser.setFileFilter(filter);
+                int value = chooser.showSaveDialog(null);
+                if (value == JFileChooser.APPROVE_OPTION) {
+                    file = chooser.getSelectedFile();
+                }
+
             }
+            FileWriter fw = new FileWriter(file, true);
+            String board = "";
+            for (int i = 0; i < SIDE * SIDE; i++) {
+                board += _board.get(i).shortName();
+            }
+            fw.write(board + "\n");
+            fw.write(_board.whoseMove().shortName());
+            fw.flush();
+            fw.close();
         } catch (IOException e) {
-            System.err.println("Wrong writing.");
+            /* Ignore IOException */
         }
     }
 
     /** Perform the command 'Load OPERANDS[0]'. */
     private void doLoad(String[] operands) {
         try {
-            InputStream helpIn =
-                    Game.class.getClassLoader().getResourceAsStream(operands[0] + ".game");
-            if (helpIn == null) {
-                System.err.println("The game \"" + operands[0] +
-                        "\" does not exist." );
+            FileReader fr = null;
+            if (_gui == null) {
+                InputStream helpIn =
+                        Game.class.getClassLoader().getResourceAsStream(operands[0] + ".game");
+                if (helpIn == null) {
+                    System.err.println("The game \"" + operands[0] +
+                            "\" does not exist." );
+                } else {
+                    fr = new FileReader(operands[0] + ".game");
+                }
             } else {
-               FileReader fr = new FileReader(operands[0] + ".game");
-               BufferedReader br = new BufferedReader(fr);
-               String board = br.readLine();
-               String whosemove = br.readLine();
-               if (whosemove.equals("b")) {
-                   _board.setPieces(board, BLACK);
-               } else {
-                   _board.setPieces(board, WHITE);
-               }
-               br.close();
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                        "game documents", "game");
+                chooser.setFileFilter(filter);
+                int value = chooser.showOpenDialog(null);
+                if (value == JFileChooser.APPROVE_OPTION) {
+                    File file = chooser.getSelectedFile();
+                    fr = new FileReader(file);
+                } else {
+                    JOptionPane.showMessageDialog(null, "No file has been selected.");
+                }
+            }
+            if (fr != null) {
+                BufferedReader br = new BufferedReader(fr);
+                String board = br.readLine();
+                String whosemove = br.readLine();
+                if (whosemove.equals("b")) {
+                    _board.setPieces(board, BLACK);
+                } else {
+                    _board.setPieces(board, WHITE);
+                }
+                fr.close();
+                br.close();
             }
         } catch (IOException e) {
-            System.err.println("Wrong reading.");
+            /* Ignore IOException */
         }
     }
 
@@ -271,10 +305,14 @@ class Game {
 
     /** Perform the command 'Status'. */
     private void doStatus(String[] unused) {
-        System.out.println("===");
-        System.out.println("Black: " + (_blackIsManual? "Manual":"AI"));
-        System.out.println("White: " + (_whiteIsManual? "Manual":"AI"));
-        System.out.println("===");
+        if (_gui == null) {
+            System.out.println("Black: " + (_blackIsManual? "Manual":"AI"));
+            System.out.println("White: " + (_whiteIsManual? "Manual":"AI"));
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "Black: " + (_blackIsManual? "Manual":"AI") + "\n" +
+                            "White: " + (_whiteIsManual? "Manual":"AI"));
+        }
     }
 
     /** Report the outcome of the current game. */
